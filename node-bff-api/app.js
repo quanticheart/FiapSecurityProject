f151ee21174457761b3c950c49a7c8540be5d27e
jonnyz <dev.jonny255d@gmail.com>
2022-01-22 18:33:08 -0300

    added crt in project

--- node-bff-api/app.js
+++ node-bff-api/app.js
@@ -32,39 +32,3 @@ app.post('/buy', async (req, res) => {
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
 });
-
-// require('utils');
-// const request = require('request');
-//
-// const app = getApp()
-// const port = 3000
-//
-// app.get('/products', async (req, res, next) => {
-//     request(`${host}:3001/products`, function(err, body){
-//         return res.json(JSON.parse(body.body));
-//     });
-// });
-//
-// app.post('/buy', async (req, res, next) => {
-//     request({
-//         url: `${host}:3002/orders`,
-//         headers: {'content-type' : 'application/json'},
-//         method: 'POST',
-//         body: JSON.stringify(req.body)
-//     }, function(error, response, body){
-//         if(error) {
-//             console.log(error);
-//         } else {
-//             console.log(response.statusCode, body);
-//             let resp = JSON.parse(body);
-//             resp.status = response.statusCode;
-//             return res.json(resp);
-//         }
-//     });
-// });
-//
-//
-// app.listen(port, () => {
-//     console.log(`Listening at http://localhost:${port}`)
-// });
-
--- node-bff-api/utils.js
+++ node-bff-api/utils.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const RateLimit = require('express-rate-limit');
+const https = require('https');
+const fs = require('fs');
 
 exports.host = process.env.DOCKER_HOST_IP || 'http://localhost';
 exports.getApp = function () {
@@ -20,6 +22,12 @@ exports.getApp = function () {
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(express.json());
     app.use(cookieParser());
+
+    // let privateKey = fs.readFileSync('./sslcert/selfsigned.key', 'utf8');
+    // let certificate = fs.readFileSync('./sslcert/selfsigned.crt', 'utf8');
+    // let credentials = {key: privateKey, cert: certificate};
+    // let httpsServer = https.createServer(credentials, app);
+    // httpsServer.listen(3000);
 
     return app;
 }
