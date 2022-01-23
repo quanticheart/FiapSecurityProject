315c8018a8ab387e0df7f56e9a70b274ba3d9318
Alan Silva <alan.bezerra@xpi.com.br>
2022-01-23 00:46:03 -0300

    feature: Ajusta clientID e domain do Auth0

--- angular-spa/src/app/auth/auth0-variables.ts
+++ angular-spa/src/app/auth/auth0-variables.ts
@@ -8,8 +8,8 @@ interface AuthConfig {
 }
 
 export const AUTH_CONFIG: AuthConfig = {
-  clientID: 'aAWdxLNf3uIiyorXTEa5fv6XLuWJc7jM',
-  domain: 'dev-aivd9uma.us.auth0.com',
+  clientID: 'Y9WWSwFSwUkeZNiBPfsdCg21Sf7qRoex',
+  domain: 'dev-5b04zxtm.us.auth0.com',
   callbackURL: 'http://localhost:4200/callback',
   silentCallbackURL: 'http://localhost:4200/silent',
   audience: 'http://localhost:4200',
