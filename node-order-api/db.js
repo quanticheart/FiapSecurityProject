efdfa5c389d042feafa250015a63f74ea32e0440
jonnyz <dev.jonny255d@gmail.com>
2022-01-29 19:58:57 -0300

    remove all console log from db.js in project

--- node-order-api/db.js
+++ node-order-api/db.js
@@ -1,7 +1,7 @@
-async function connect(){
-    if(global.connection && global.connection.state !== 'disconnected')
+async function connect() {
+    if (global.connection && global.connection.state !== 'disconnected')
         return global.connection;
- 
+
     const mysql = require("mysql2/promise");
     const connection = await mysql.createConnection({
         host: process.env.DB_HOST || 'localhost',
@@ -10,82 +10,72 @@ async function connect(){
         password: 'test',
         database: 'finalProject',
         multipleStatements: true
-      } );
-    console.log("Conectou no MySQL!");
+    });
     global.connection = connection;
     return connection;
 }
 
-async function getAllOrders(){
+async function getAllOrders() {
     const conn = await connect();
-    
-    const query = `SELECT * FROM orders LIMIT 1000;`;
-    console.log(`Executando query: ${query}`);
-
+    const query = `SELECT *
+                   FROM orders LIMIT 1000;`;
     const [rows, fields] = await connection.execute(query);
-    console.log(`Rows: ${JSON.stringify(rows)}`);
     return rows;
 }
 
-async function getOrderById(id){
+async function getOrderById(id) {
     const conn = await connect();
-    
-    const query = `SELECT * FROM orders WHERE id = "${id}";`;
-    console.log(`Executando query: ${query}`);
-    
+    const query = `SELECT *
+                   FROM orders
+                   WHERE id = "${id}";`;
     const [rows, fields] = await connection.execute(query);
-
     return rows;
 }
 
-async function getOrderByClientId(id){
+async function getOrderByClientId(id) {
     const conn = await connect();
-    
-    const query = `SELECT * FROM orders WHERE client_id = "${id}";`;
-    console.log(`Executando query: ${query}`);
-    
+    const query = `SELECT *
+                   FROM orders
+                   WHERE client_id = "${id}";`;
     const [rows, fields] = await connection.execute(query);
-
     return rows;
 }
 
-async function updateOrderById(id, clientId, productId, amount){
-    try{
+async function updateOrderById(id, clientId, productId, amount) {
+    try {
         const conn = await connect();
-    
-        const query = `UPDATE orders SET client_id = "${clientId}", product_id = "${productId}", amount = ${amount} WHERE id = "${id}";`;
-        console.log(`Executando query: ${query}`);
-        
+        const query = `UPDATE orders
+                       SET client_id  = "${clientId}",
+                           product_id = "${productId}",
+                           amount     = ${amount}
+                       WHERE id = "${id}";`;
         const [rows] = await conn.execute(query);
         return rows;
-    }catch(err){
+    } catch (err) {
         throw {code: 500, message: 'Erro inesperado ao tentar cadastrar pedido'};
     }
 }
 
-async function deleteOrderById(id){
+async function deleteOrderById(id) {
     const conn = await connect();
-    
-    const query = `DELETE FROM orders WHERE id = "${id}";`;
-    console.log(`Executando query: ${query}`);
-
+    const query = `DELETE
+                   FROM orders
+                   WHERE id = "${id}";`;
     await connection.execute(query);
 }
 
-async function insertOrder(id, clientId, productId, amount){
+async function insertOrder(id, clientId, productId, amount) {
     const conn = await connect();
-
-    const query = `INSERT INTO orders(id, client_id, product_id, amount) VALUES ("${id}", "${clientId}", "${productId}", ${amount});`;
-    console.log(`Executando query: ${query}`);
-
-    try{
+    const query = `INSERT INTO orders(id, client_id, product_id, amount)
+                   VALUES ("${id}", "${clientId}", "${productId}", ${amount});`;
+    try {
         await connection.execute(query);
-    }catch(err){
-        if(err.errno === 1062){
+    } catch (err) {
+        if (err.errno === 1062) {
             throw {code: 400, message: 'Já existe um pedido cadastrado com este id!'};
-        }else if(err.errno === 1452){
+        } else if (err.errno === 1452) {
             throw {code: 400, message: 'Produto não encontrado!'};
-        }else{
+        } else {
             throw {code: 500, message: 'Erro inesperado ao tentar cadastrar pedido'};
         }
     }
--- node-product-api/db.js
+++ node-product-api/db.js
@@ -1,9 +1,9 @@
-const { randomUUID } = require('crypto');
+const {randomUUID} = require('crypto');
 
-async function connect(){
-    if(global.connection && global.connection.state !== 'disconnected')
+async function connect() {
+    if (global.connection && global.connection.state !== 'disconnected')
         return global.connection;
- 
+
     const mysql = require("mysql2/promise");
     const connection = await mysql.createConnection({
         host: process.env.DB_HOST || 'localhost',
@@ -12,70 +12,62 @@ async function connect(){
         password: 'test',
         database: 'finalProject',
         multipleStatements: true
-      } );
-    console.log("Conectou no MySQL!");
+    });
     global.connection = connection;
     return connection;
 }
 
-async function getAllProducts(){
+async function getAllProducts() {
     const conn = await connect();
-    
-    const query = `SELECT * FROM products LIMIT 1000;`;
-    console.log(`Executando query: ${query}`);
-
+    const query = `SELECT *
+                   FROM products LIMIT 1000;`;
     const [rows, fields] = await connection.execute(query);
-    console.log(`Rows: ${JSON.stringify(rows)}`);
     return rows;
 }
 
-async function getProductById(id){
+async function getProductById(id) {
     const conn = await connect();
-    
-    const query = `SELECT * FROM products WHERE id = "${id}";`;
-    console.log(`Executando query: ${query}`);
-    
+    const query = `SELECT *
+                   FROM products
+                   WHERE id = "${id}";`;
     const [rows, fields] = await connection.execute(query);
-
     return rows;
 }
 
 
-async function updateProductById(id, name, description, value){
-    try{
+async function updateProductById(id, name, description, value) {
+    try {
         const conn = await connect();
-    
-        const query = `UPDATE products SET name = "${name}", description = "${description}", value = ${value} WHERE id = "${id}";`;
-        console.log(`Executando query: ${query}`);
-        
+        const query = `UPDATE products
+                       SET name        = "${name}",
+                           description = "${description}",
+                           value       = ${value}
+                       WHERE id = "${id}";`;
         const [rows] = await conn.execute(query);
         return rows;
-    }catch(err){
+    } catch (err) {
         throw {code: 500, message: 'Erro inesperado ao tentar cadastrar usuário'};
     }
 }
 
-async function deleteProductById(id){
+async function deleteProductById(id) {
     const conn = await connect();
-    
-    const query = `DELETE FROM products WHERE id = "${id}";`;
-    console.log(`Executando query: ${query}`);
-
+    const query = `DELETE
+                   FROM products
+                   WHERE id = "${id}";`;
     await connection.execute(query);
 }
 
-async function insertProduct(name, description, value){
+async function insertProduct(name, description, value) {
     const conn = await connect();
-
-    const query = `INSERT INTO products(id, name, description, value) VALUES ("${randomUUID()}", "${name}", "${description}", ${value});`;
-    console.log(`Executando query: ${query}`);
-
-    try{
+    const query = `INSERT INTO products(id, name, description, value)
+                   VALUES ("${randomUUID()}", "${name}", "${description}", ${value});`;
+    try {
         await connection.execute(query);
-    }catch(err){
-        if(err.errno === 1062){
+    } catch (err) {
+        if (err.errno === 1062) {
             throw {code: 400, message: 'Já existe um producte cadastrado com este usuário!'};
-        }else{
+        } else {
             throw {code: 500, message: 'Erro inesperado ao tentar cadastrar usuário'};
         }
     }
